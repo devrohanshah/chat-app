da3d9b976084824e667079310626d84be5c8f308
Rohan Shah <140335546+devrohanshah@users.noreply.github.com>
2024-06-26 21:28:18 +0545

    Update server.js

--- server.js
+++ server.js
@@ -2,12 +2,15 @@ const express = require("express");
 const http = require("http");
 const socketIo = require("socket.io");
 const sqlite3 = require("sqlite3").verbose();
+const path = require("path");
 const fs = require("fs");
 
 const app = express();
 const server = http.createServer(app);
 const io = socketIo(server);
-const db = new sqlite3.Database("chat.db"); // Persistent file-based database
+
+const dbPath = path.join(__dirname, ".render", "chat.db");
+const db = new sqlite3.Database(dbPath); // Persistent file-based database
 
 // Create tables for storing messages and users
 db.serialize(() => {
@@ -27,26 +30,32 @@ io.on("connection", (socket) => {
   console.log("a user connected");
 
   socket.on("join room", ({ username, room }) => {
-    socket.username = username;
-    socket.room = room;
     socket.join(room);
 
     // Store username in the database if it doesn't exist
-    db.run("INSERT OR IGNORE INTO users (username) VALUES (?)", [username], (err) => {
-      if (err) {
-        console.error(err.message);
+    db.run(
+      "INSERT OR IGNORE INTO users (username) VALUES (?)",
+      [username],
+      (err) => {
+        if (err) {
+          console.error(err.message);
+        }
       }
-    });
+    );
 
     // Retrieve old messages from the database
-    db.all("SELECT rowid, username, message, type, content, timestamp FROM messages WHERE room = ? ORDER BY timestamp", [room], (err, rows) => {
-      if (err) {
-        console.error(err.message);
-        return;
+    db.all(
+      "SELECT rowid, username, message, type, content, timestamp FROM messages WHERE room = ? ORDER BY timestamp",
+      [room],
+      (err, rows) => {
+        if (err) {
+          console.error(err.message);
+          return;
+        }
+        // Send old messages to the user who just joined
+        socket.emit("old messages", rows);
       }
-      // Send old messages to the user who just joined
-      socket.emit("old messages", rows);
-    });
+    );
 
     // Notify room about the new user
     io.to(room).emit("chat message", {
@@ -57,43 +66,45 @@ io.on("connection", (socket) => {
 
   socket.on("chat message", ({ room, username, message, type, content }) => {
     // Save message to the database
-    db.run("INSERT INTO messages (room, username, message, type, content) VALUES (?, ?, ?, ?, ?)", [room, username, message, type, content], (err) => {
-      if (err) {
-        console.error(err.message);
-        return;
+    db.run(
+      "INSERT INTO messages (room, username, message, type, content) VALUES (?, ?, ?, ?, ?)",
+      [room, username, message, type, content],
+      function (err) { // Changed to function to access `this.lastID`
+        if (err) {
+          console.error(err.message);
+          return;
+        }
+        // Broadcast message to the room
+        io.to(room).emit("chat message", { rowid: this.lastID, username, message, type, content });
       }
-      // Broadcast message to the room
-      io.to(room).emit("chat message", { username, message, type, content });
-    });
+    );
   });
 
   socket.on("delete message", ({ messageId }) => {
-    const username = socket.username; // Get the username associated with this socket
-    const room = socket.room; // Get the room associated with this socket
+    db.get(
+      "SELECT username FROM messages WHERE rowid = ?",
+      [messageId],
+      (err, row) => {
+        if (err) {
+          console.error(err.message);
+          return;
+        }
 
-    // Get the username associated with the message
-    db.get("SELECT username FROM messages WHERE rowid = ?", [messageId], (err, row) => {
-      if (err) {
-        console.error(err.message);
-        return;
+        // Check if the user requesting deletion is the message sender
+        if (row && row.username === username) {
+          db.run("DELETE FROM messages WHERE rowid = ?", [messageId], (err) => {
+            if (err) {
+              console.error(err.message);
+              return;
+            }
+            // Notify room about the deleted message
+            io.to(room).emit("message deleted", { messageId });
+          });
+        } else {
+          console.log("Unauthorized deletion attempt");
+        }
       }
-
-      // Check if the user requesting deletion is the message creator
-      if (row && row.username === username) {
-        // User is allowed to delete the message
-        db.run("DELETE FROM messages WHERE rowid = ?", [messageId], (err) => {
-          if (err) {
-            console.error(err.message);
-            return;
-          }
-          // Notify room about the deleted message
-          io.to(room).emit("message deleted", { messageId });
-        });
-      } else {
-        // User is not authorized to delete this message
-        console.log("Unauthorized deletion attempt");
-      }
-    });
+    );
   });
 
   socket.on("disconnect", () => {
